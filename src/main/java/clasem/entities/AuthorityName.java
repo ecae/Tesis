b00dc846e692390abfd24329c505c0fec2973ba0
Erick Cruz Estrada <eric_cruz_estrada@hotmail.com>
2017-07-19 15:26:55 -0500

    Configuracion inicial del Proyecto

---
+++ src/main/java/clasem/entities/AuthorityName.java
@@ -0,0 +1,5 @@
+package clasem.entities;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/clasem/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package clasem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import clasem.entities.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
